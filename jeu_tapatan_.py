9579e66ae5a9a24f56fd3033c2bb6eb65d76f4d4
Nabila ELAIDOUDI <nabila.elaidoudi@ens.uvsq.fr>
2021-05-08 20:43:45 +0200

    new

--- jeu_tapatan_.py
+++ jeu_tapatan_.py
@@ -11,39 +11,43 @@
 
 #####################
 #import des modules
-import tkinter as tk 
+from tkinter import *
 
 #####################
 # constantes
-LARGEUR= 500
-HAUTEUR= 500
+LARGEUR= 300
+HAUTEUR= 400
 
 
 #####################
 # fonctions
 
+def tracePlateau():
+    cadre.create_rectangle(50,100,250,300,width=5)
+    cadre.create_line(50,100,250,300,width=5,fill="black")
+    cadre.create_line(50,300,250,100,width=5,fill="black")
+    cadre.create_line(50,200,250,200,width=5,fill="black")
+    cadre.create_line(150,100,150,300,width=5,fill="black")
+    for i in range (15) : 
+        xt,yt=i%3*100+50, i//3*100 
+        if i<3:yt+=50
+        if i>11:yt-=50
+        cadre.create_oval(xt-7, yt-7, xt+7, yt+7, width=5, fill="black")
+        cadre.create_oval(xt-3, yt-3, xt+3, yt+3, width=0, fill="light yellow")
+
 #####################
 # programme principal
-racine = tk.Tk() #ouvre une fenetre 
-
-canvas = tk.Canvas(racine, bg="pink", width= LARGEUR  , height= HAUTEUR )
-canvas.grid(row=1 , column=1 )
-
-canvas.create_line((250, 0),(250, 500), fill = "white") #ligne de l'axe vertical 
-canvas.create_line((0, 250), (500, 250),  fill="white") #ligne de l'axe horizontale
-canvas.create_line((0,0), (500, 500) , fill="white") #ligne gauche
-canvas.create_line((0,500), (500, 0) , fill="white") #ligne droite
-canvas.create_oval((0 , 0 ), (0 +20, 0 +20 ) , fill= "black") #point gauche
-canvas.create_oval((500 , 0 ), (500 -20 , 0 +20  ) , fill= "red") #point droit
-canvas1 =tk.Canvas(racine, bg="white", width=10 , height=500) #mur de gauche
-canvas1.grid(row=1 , column=0)
-canvas2 =tk.Canvas(racine, bg="white", width=500 , height=10) #mur du dessous du canvas
-canvas2.grid(row=2 , column=1)
-canvas3 =tk.Canvas(racine, bg="white", width=10 , height=500) #mur de droite
-canvas3.grid(column=2, row=1 )
-canvas4 =tk.Canvas(racine, bg="white", width=500 , height=10) #mur du dessous du canvas
-canvas4.grid(row=0 , column=1)
-
+racine = Tk() #ouvre une fenetre 
+racine.title('Jeu Tapatan')
+cadre = Canvas(racine, bg="light yellow", width= LARGEUR  , height= HAUTEUR )
+cadre.grid(row=1 , column=1,columnspan = 3)
+tracePlateau()
+photo1 = PhotoImage(file ='pion1.gif')
+photo2 = PhotoImage(file = 'pion2.gif')
+pion0,pion1 = [], []
+for i in range(3):
+    pion0.append(cadre.create_image(i*100+50,50, image = photo1))
+    pion1.append(cadre.create_image(i*100+50,350, image = photo2))
 
 
 racine.mainloop() #ferme une fenetre 
