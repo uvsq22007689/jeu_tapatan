2b9c4f98e8536c21e904eb6a9df4003b0204b085
Nabila ELAIDOUDI <nabila.elaidoudi@ens.uvsq.fr>
2021-05-24 21:36:22 +0200

    last

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # jeu_tapatan
+
+Un jeu simple à deux joueurs dont l'un des joueurs est l'ordinateur(IA) et qui
+nécessite une forme d'intelligence de sa part : 
+
+le jeu des neuf trous appelé aussi ' jeu du tapatan 'est une sorte de plateau de jeu formant
+un Carrée de 3 sur 3, chaque joueur dispose de trois pions qu'il doit aligner sur le plateau. 
+Au début, chacun à tour de rôle pose un pion, n'importe où sur le plateau. Une fois les six pions posés, chacun
+peut déplacer un de ses pions d'une position, selon un des tracés figurés sur le plateau (le long des six
+lignes ou des deux diagonales), si le nouvel emplacement est libre.
+  
+
+On dessine le plateau à l'aide d'un Canvas . Les pions sont créés à partir de deux
+images photo1 et photo2 (au format gif) qui sont utilisées pour definir des variables pion0[0], pion0[1] et pion0[2] pour les trois pions de l'ordinateur (désigné par 0 pour ordinateur) et pion1[i] pour ceux de l'humain (i variant de 0 à 2 pour l'humain désigné par 1).
+L'emplacement de ces pions est réglé par les coordonnées qui sont initialisées :
+pion0.append(cadre.create_image(i*100+50,50,image=photo1))#ordinateur
+pion1.append(cadre.create_image(i*100+50,350,image=photo2))#humain
+
+Le mouvement des pions est lié aux mouvements de la souris en 3 étapes : 
+
+ -le clic déclenche la fonction clic() qui détecte que le pion *choixPion* a été sélectionné (on ne s'intéresse qu'aux pions de l'humain car l'ordinateur bouge lui-même ses pions (automatiquement)) en acceptant un certain domaine de variation des coordonnées du clic (x.event et y.event) autour de coordonnées du pion.
+
+ -le déplacement de la souris, bouton de gauche enfoncé, lance la fonction detection() qui redessine le pion lors de son déplacement
+
+ -le relâchement de la souris lance la fonction lache() qui examine le nouvel emplacement, fixe le pion à cet
+emplacement s'il est vide et s'il correspond à un placement valide
+
+Le bouton « Rejouer » permet de rejouer une partie, ce qui repositionne les pions à l'extérieur du plateau et réinitialise les variables qui doivent l'être. 
+
+On a crée une fonction racineMessage qui permet d'avertir que si on change le mode d'utilisation ( de jouer avec l'humain à jouer avec l'ordi)
+alors les scores seront réinitialiser à 0 (car on n'utilise plus les mêmes règles) , celle ci va permettre de créer un bandeau d'avertissement.
+
+Pour effacer le bandeau d'avertissement on a crée une nouvelle fonction supprimerMessage qui permet de l'effacer, et de faire reapparaitre le plateau de jeu.
+
+Nous avons essayé de créer :
+
+-une fonction IA() qui permet à l'ordinateur d'attaquer en premier lieu pour pouvoir gagner ou de se défendre si il se sent en position de défaite. Exemple : si l'humain met son pion au milieu l'ordinateur joue sur la diagonale.
+
+-une fonction gagner() qui nous permettrai d'afficher les scores et de savoir qui des 2 joueurs auraient gagner.
+
+-une fonction humain() pour parvenir à jouer avec un autre humain tout en calculant les cases disponibles, notre piste pour y parvenir était de créer une liste imbriquée : cases = [[0,0,0],[0,0,0],[0,0,0]] et d'utiliser des structures conditionnelles pour chaque case. 
+
+-une fonction lache_du_pion() qui permet de détecter quand l'utilisateur lache le bouton gauche de la souris et à quelle coordonées pour placer le pion au niveau des cercles noirs prédéfinis.
+
+-une fonction sauvegarde() qui doit présenter ces éléments :
+fic = open("sauvegarde", "w")
+fic.write("sauvegarde partie précédente ")
+fic.close()
+
+Mais nous n'avons pas réussi à faire ces fonctions par manque de connaissance.
--- jeu_tapatan_.py
+++ jeu_tapatan_.py
@@ -11,73 +11,61 @@
 
 #####################
 #import des modules
+import tkinter as tk
 from tkinter import *
-from random import *
+from random import randint
+import random as rd
 
 #####################
 # constantes
 LARGEUR= 300
 HAUTEUR= 400
 
-
 #####################
 # fonctions
-
-def tracePlateau():
-    cadre.create_rectangle(50,100,250,300,width=5)
-    cadre.create_line(50,100,250,300,width=5,fill="black")
-    cadre.create_line(50,300,250,100,width=5,fill="black")
-    cadre.create_line(50,200,250,200,width=5,fill="black")
-    cadre.create_line(150,100,150,300,width=5,fill="black")
-    for i in range (15) : 
-        xt,yt=i%3*100+50, i//3*100 
-        if i<3:yt+=50
-        if i>11:yt-=50
-        cadre.create_oval(xt-7, yt-7, xt+7, yt+7, width=5, fill="black")
-        cadre.create_oval(xt-3, yt-3, xt+3, yt+3, width=0, fill="light yellow")
-    
 def commencement():
-    '''cette fonction va permettre de remettre les compteurs à 0'''
-    global coup,score,avertissement,previousChoix
-    if coup!=0 and score != [0,0]:
+    """cette fonction va permettre de remettre les compteurs à 0"""
+    global coup,score,avertissement,previousChoix        
+ 
+    if coup!=0 and score!=[0,0]:
         avertissement=True
-        racineMessage('Attention! Le score va être perdu...')
+        racineMessage('Attention! Le score va etre perdu...')
 
-def racineMessage(s): 
-    '''fonction qui creer un bandeau d'avertissement pour prevenir la perte du score ''' 
-    global band,message1,message2                           
-    band = cadre.create_rectangle(5,75,298,325,width=3,fill='light yellow')    
-    message1 = cadre.create_text(150,150,text=s,font="Britannic bold 12",fill='red')
-    message2 = cadre.create_text(150,250,text='Cliquer ici pour valider ce choix'\
+def racineMessage(s):
+    """fonction qui créer un bandeau d'avertissement pour prevenir la perte du score """
+    global band,message1,message2                            
+    band=canvas.create_rectangle(5,75,298,325,width=3,fill='light yellow')    
+    message1=canvas.create_text(150,150,text=s,font="Britannic bold 12",fill='red')
+    message2=canvas.create_text(150,250,text='Cliquer ici pour valider ce choix'\
                             ,font="Britannic bold 11",fill='red')
 
-def supprimerMessage(): 
-    '''fonction va permettre d'effacer le bandeau d'avertissement et de faire 
-    reapparaitre le plateau de jeu'''
-    global coup, score,tour,band,message1,message2,avertissement
-    score = [0,0]
+def supprimerMessage():
+    """fonction va permettre d'effacer le bandeau d'avertissement et 
+    de faire reapparaitre le plateau de jeu"""
+    global coup,score,tour,band,message1,message2,avertissement   
+    score=[0,0]
     monScore.configure(text="0")
     ordiScore.configure(text="0")
-    cadre.delete(band)
-    cadre.delete(message1)
-    cadre.delete(message2)
+    canvas.delete(band)
+    canvas.delete(message1)
+    canvas.delete(message2)
     avertissement=False
     rejouer()
 
 def rejouer():
-    '''fonction permet de démarrer une nouvelle partie '''
+    """fonction permet de démarrer une nouvelle partie"""
     global coup,resteAjouer,possible,start,detectionPion,choixPion,mesPlaces\
            ,ordiPlaces,tour,gagnant,avertissement,band,message1,message2,previousChoix
     for i in range(3):
-        cadre.coords(pion0[i],i*100+50,50)
-        cadre.coords(pion1[i],i*100+50,350)
+        canvas.coords(pion0[i],i*100+50,50)
+        canvas.coords(pion1[i],i*100+50,350)
     coup,resteAjouer,possible=0,[0,1,2],[]
     start,detectionPion,choixPion=True,False,-1
     mesPlaces,ordiPlaces,gagnant=[-1,-1,-1],[-1,-1,-1],-1
     if avertissement:
-        cadre.delete(band)                                         
-        cadre.delete(message1)
-        cadre.delete(message2)
+        canvas.delete(band)                                          
+        canvas.delete(message1)
+        canvas.delete(message2)
         choixDebut.set(previousChoix)
         avertissement=False
     previousChoix=choixDebut.get()
@@ -86,37 +74,72 @@ def rejouer():
     if choixDebut.get()==0:
         tour=joueurDebut[0]
     elif choixDebut.get()==1:
-        tour=joueurDebut[randint(0,2)]
+        tour=joueurDebut[randint(0,1)]
     else:tour=joueurDebut[2]
     message.configure(text="à {} de jouer".format(tour))
-    if tour=="l'ordi": 
-        ordinateur()
-    print('\nNouvelle partie')
 
-def ordinateur():
+def IA(): 
     pass
 
-#####################
+def clic(event):
+    """fonction qui permet de detecter que le pion a ete selctionner
+     pour le deplacement"""
+    global detectionPion,choixPion,xDeb,yDeb,gagnant,avertissement
+    x1,y1,detectionPion=event.x,event.y,False
+    if avertissement and 0<x1<300 and 70<y1<330: supprimerMessage()         
+    if tour=="l'humain" and gagnant==-1:
+        for i in range(3):
+            [xDeb,yDeb]=canvas.coords(pion1[i])
+            if (x1-xDeb)**2+(y1-yDeb)**2<400:#estimation du rayon du pion à 20
+                choixPion=i
+                if start and resteAjouer.count(choixPion)==0 :
+                    choixPion=-1
+                    break
+                detectionPion=True
+                break
+
+def detection(event):
+    """fonction que redessine le deplacement du pion entre autre la detection 
+    de son deplacement"""
+    x1,y1=event.x,event.y
+    if detectionPion:
+        if x1<0:x1=0
+        if x1>300:x1=300                      
+        if y1<50:y1=50
+        if y1>350:x1=350
+        canvas.coords(pion1[choixPion],x1,y1)
+
+def lache_du_pion(event):
+    pass
+
+def humain():
+    pass
+
+def gagner():
+    pass
+
 # programme principal
-racine = Tk() #ouvre une fenetre 
+racine = tk.Tk() #ouvre une fenetre 
 racine.title('Jeu Tapatan')
-cadre = Canvas(racine, bg="light yellow", width= LARGEUR  , height= HAUTEUR )
-cadre.grid(row=1 , column=1,columnspan = 3)
-tracePlateau()
-photo1 = PhotoImage(file ='pion1.gif')
-photo2 = PhotoImage(file = 'pion2.gif')
-pion0,pion1 = [], []
-joueurDebut=["l'ordi","aleatoire","l'humain"]
-tour=joueurDebut[randint(0,2)]
-for i in range(3):
-    pion0.append(cadre.create_image(i*100+50,50, image = photo1))
-    pion1.append(cadre.create_image(i*100+50,350, image = photo2))
+joueurDebut=["IA","l'humain"]
+tour=joueurDebut[randint(0,1)]
+canvas = tk.Canvas(racine, bg="pink", width= LARGEUR  , height= HAUTEUR )
+canvas.grid(row=1,column=1,columnspan=3)
+canvas.bind("<Button-1>",clic)
+canvas.bind("<B1-Motion>",detection)
 
-pion0,pion1 = [], []
-for i in range(3):
-    pion0.append(cadre.create_image(i*100+50,50, image = photo1))
-    pion1.append(cadre.create_image(i*100+50,350, image = photo2))
+canvas.create_line(50,100,250,300, fill = "white") #ligne de l'axe vertical 
+canvas.create_line(50,300,250,100,  fill="white") #ligne de l'axe horizontale
+canvas.create_line(50,200,250,200 , fill="white") #ligne gauche
+canvas.create_line(150,100,150,300 , fill="white") #ligne droite
 
+for i in range(15):
+        xt,yt=i%3*100+50,i//3*100
+        if i<3:yt+=50
+        if i>11:yt-=50
+        
+        canvas.create_oval(xt-7,yt-7,xt+7,yt+7,width=5,fill='black')
+        canvas.create_oval(xt-3,yt-3,xt+3,yt+3,width=0,fill='pink')
 ordiScore=Label(racine,text="0")
 ordiScore.grid(row=2,column=1)
 message=Label(racine,text="à {} de jouer".format(tour))
@@ -126,30 +149,27 @@ monScore.grid(row=2,column=3)
 choixDebut=IntVar()
 choixDebut.set(1)
 
-for i in range(3): # Création des trois 'boutons radio' :
-    bouton=Radiobutton(racine,text= joueurDebut[i],variable=choixDebut,
+for i in range(2): # Création des trois 'boutons radio' :
+    bouton=Radiobutton(racine,text=joueurDebut[i],variable=choixDebut,
                        value=i,command=commencement)
     bouton.grid(row=3,column=i+1)
-
-bouton=Button(racine,text="Rejouer")
+bouton=Button(racine,text="Rejouer", command=rejouer)
 bouton.grid(row=4,column=1,columnspan=3)
+photo1 = PhotoImage(file ='pion1.gif')
+photo2 = PhotoImage(file = 'pion2.gif')
+pion0,pion1 = [], []
 
-#initialisation des variables globales
+for i in range(3):# Création des six pions :
+    pion0.append(canvas.create_image(i*100+50,50,image=photo1))#IA
+    pion1.append(canvas.create_image(i*100+50,350,image=photo2))#humain
+  
+#initialisations des variables globales
 start,detectionPion,avertissement=True,False,False
 possible,coup,score,resteAjouer=[],0,[0,0],[0,1,2]
 mesPlaces,ordiPlaces,choixPion=[-1,-1,-1],[-1,-1,-1],-1
 xDeb,yDeb,gagnant,previousChoix=0,0,-1,1
 band,message1,message2=None,None,None
 
-
-for i in range(3):# Création des six pions :
-    pion0.append(cadre.create_image(i*100+50,50,image=photo1))#ordi
-    pion1.append(cadre.create_image(i*100+50,350,image=photo2))#humain
-if tour == "l'ordi": 
-    ordinateur()
-
-
-
-racine.mainloop() #ferme une fenetre 
+racine.mainloop()
 
 
